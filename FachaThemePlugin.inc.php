6026261a2213debf2e821b3debbbb018b55c4bc7
tocteman <juanfrancisco.vinueza@gmail.com>
2022-09-08 10:24:03 -0500

    cambios imports

--- FachaThemePlugin.inc.php
+++ FachaThemePlugin.inc.php
@@ -1,24 +1,24 @@
 <?php
 
-import('lib.pkp.classes.plugins.ThemePlugin')
+import('lib.pkp.classes.plugins.ThemePlugin');
 
-class FachaThemePlugin extends ThemePlugin
-{
-    public function isActive()
-    {
-        return parent::isActive();
-    }
-    public function init()
-    {
-        $this->addStyle('stylesheet', 'styles/index.less');
-    }
-    public function getDisplayName()
-    {
-        return __('Facha Theme');
-    }
-    public function getDescription()
-    {
-        return __('seguimos');
-    }
+class FachaThemePlugin extends ThemePlugin {
+
+  public function isActive() {
+    if (defined('SESSION_DISABLE_INIT')) return true;
+    return parent::isActive();
+  }
+  public function init()
+  {
+    $this->addStyle('stylesheet', 'styles/index.less');
+  }
+  public function getDisplayName()
+  {
+    return __('FachaTheme');
+  }
+  public function getDescription()
+  {
+    return __('seguimos');
+  }
 
 }
